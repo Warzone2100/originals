093b8faa06c435787d9ad22c59e4deb7e4b1cf38
elio <elio@wz2100.net>
2008-08-13 19:57:57 +0000

    propulsion prhnaval1 finished, with teamcolors and texture
    --HG--
    extra : convert_revision : svn%3A4a71c877-e1ca-e34f-864e-861f7616d084/originals%405806

---
+++ naval/prhnaval1/obj2pie-script.sh
@@ -0,0 +1,9 @@
+SCALE="60"
+obj2pie -s $SCALE -y -r prhnaval1_green.obj prhnaval1_green.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_orange.obj prhnaval1_orange.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_grey.obj prhnaval1_grey.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_black.obj prhnaval1_black.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_red.obj prhnaval1_red.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_blue.obj prhnaval1_blue.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_purple.obj prhnaval1_purple.pie page-28-naval.png
+obj2pie -s $SCALE -y -r prhnaval1_cyan.obj prhnaval1_cyan.pie page-28-naval.png
